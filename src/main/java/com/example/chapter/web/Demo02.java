0f4f9e479337dc83b53e5e66aea9cf1d0251fedd
17611487238 <734777215@qq.com>
2024-06-18 15:29:41 +0800

    增加了02节临时内容

--- src/main/java/com/example/chapter/web/Demo02.java
+++ src/main/java/com/example/chapter/web/Demo02.java
@@ -14,35 +14,109 @@ public class Demo02 {
     private static final Logger log = LoggerFactory.getLogger(Demo02.class);
 
     /**
-     * 错误用法01
+     * 错误用法01 打印方式不对
      * @return
      */
     @RequestMapping(value = "error1")
     public String error1(){
         String result = "文科生说Java";
-        System.out.println("错误用法01:------------ " + result);
+        System.out.println("错误用法01 欢迎来到:------------ " + result);
         return "SUCESS";
     }
 
     /**
-     * 错误用法02
+     * 错误用法02 日志级别调用不对
      * @return
      */
     @RequestMapping(value = "error2")
     public String error2(){
         String result = "文科生说Java";
-        log.trace("错误用法02:------------ {}" + result);
+        log.trace("错误用法02 trace 欢迎来到:------------ " + result);
+        log.debug("错误用法03 debug 欢迎来到:------------ " + result);
         return "SUCESS";
     }
 
     /**
-     * 错误用法03
+     * 错误用法03,catch 错误用法
      * @return
      */
     @RequestMapping(value = "error3")
     public String error3(){
+        try{
+            int a = 1;
+            int b = 0;
+            int i = a / b;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "SUCESS";
+    }
+
+    /**
+     * 错误用法04,没能打印完整的错误信息
+     * @return
+     */
+    @RequestMapping(value = "error4")
+    public String error4(){
+        try{
+            int a = 1;
+            int b = 0;
+            int i = a / b;
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+        return "SUCESS";
+    }
+
+    /**
+     * 错误用法05,打印写法导致性能问题
+     * @return
+     */
+    @RequestMapping(value = "error5")
+    public String error5(){
+        int a = 1;
+        int b = 2;
+        log.info("错误用法05: 关键变量:" + a + " 关键变量:" + b);
+        return "SUCESS";
+    }
+
+    /**
+     * 正确用法01,解决打印方法调用不对,和调用级别的问题
+     * @return
+     */
+    @RequestMapping(value = "correct1")
+    public String correct1(){
         String result = "文科生说Java";
-        log.debug("错误用法03:------------ {}" + result);
+        log.info("正确用法01 info 欢迎来到:------------ {}",result);
+        log.error("正确用法01 error 欢迎来到:------------ {}",result);
+        return "SUCESS";
+    }
+
+    /**
+     * 正确用法02,解决打印不全的问题
+     * @return
+     */
+    @RequestMapping(value = "correct2")
+    public String correct2(){
+        try{
+            int a = 1;
+            int b = 0;
+            int i = a / b;
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+        }
+        return "SUCESS";
+    }
+
+    /**
+     * 正确用法03,解决打印性能的问题
+     * @return
+     */
+    @RequestMapping(value = "correct3")
+    public String correct3(){
+        int a = 1;
+        int b = 2;
+        log.info("正确用法03: 关键变量:{} 关键变量:{}", a, b);
         return "SUCESS";
     }
 }
